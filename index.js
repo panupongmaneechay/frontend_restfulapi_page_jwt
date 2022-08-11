c07d4fc7386ec39b8ac0a0a0767cc765850f6233
Panupong.Ma <panupong.ma@bdms.co.th>
2022-08-11 16:39:57 +0700

    add post profile

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ if (jwt == null) {
 
 function loadUser() {
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://www.mecallapi.com/api/auth/user");
+  xhttp.open("GET", "http://localhost:8081/token/v1/article/get");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhttp.setRequestHeader("Authorization", "Bearer "+jwt);
+  xhttp.setRequestHeader("token",jwt);
   xhttp.send();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
-      if (objects["status"] == "ok") {
+      if (objects["status"] == "success") {
         const user = objects["user"]
         document.getElementById("fname").innerHTML = user["fname"];
         document.getElementById("avatar").src = user["avatar"];
-        document.getElementById("username").innerHTML = user["username"];
+        document.getElementById("username").innerHTML = "Username : " + user["username"];
+        // document.getElementById("username").innerHTML = "username : "+"panupong"
       }
     }
   };
