269ab31ceeca778da2fa2caedd59354366ffec45
panupongmaneechay <panupongmaneechay@hotmail.com>
2022-08-15 16:17:53 +0700

    add for loop html

--- index.js
+++ index.js
@@ -14,16 +14,24 @@ function loadUser() {
   xhttp.send();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4) {
+      
       const objects = JSON.parse(this.responseText);
       if (objects["status"] == "success") {
-        const user = objects["user"]
         // document.getElementById("fname").innerHTML = user["fname"];
         document.getElementById("fname").innerHTML = username;
-        document.getElementById("article").innerHTML = objects["article"];
-        document.getElementById("updateuser").innerHTML = "create when "+objects["createupdate"]+ "  by " + objects["updateusername"];
-        document.getElementById("avatar").src = user["avatar"];
-        
-        // document.getElementById("username").innerHTML = "username : "+"panupong"
+        if(objects["recordset"].length > 0){
+          var updateuser = document.getElementById("article")
+          for(let i = 0; objects["recordset"].length > i; i++){
+            // document.getElementById("article").innerHTML = objects["recordset"][i]["article"]  ;
+            // document.getElementById("updateuser").innerHTML = "create when "+objects["recordset"][i]["createupdate"]+ "  by " + objects["recordset"][i]["updateusername"];
+            updateuser.innerHTML += ((i+1)+"."+" "+
+            objects["recordset"][i]["article"] + "<br />"+"• create when "+
+            objects["recordset"][i]["createupdate"]+ "  by " + 
+            objects["recordset"][i]["updateusername"] + "<br />")
+
+          }
+        }
+       
       }
     }
   };
@@ -36,3 +44,4 @@ function logout() {
   localStorage.removeItem("username");
   window.location.href = './login.html'
 }
+
