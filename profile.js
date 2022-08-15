756e619b354d1cb2d19a943b3f4ac1b325b8c6a6
panupongmaneechay <panupongmaneechay@hotmail.com>
2022-08-15 21:46:04 +0700

    add pi

--- profile.js
+++ profile.js
@@ -1,44 +1,43 @@
 var jwt = localStorage.getItem("jwt");
-var username = localStorage.getItem("username");
+const username = localStorage.getItem("username");
 var fname = localStorage.getItem("firstname");
 var lname = localStorage.getItem("lastname");
 var tel = localStorage.getItem("tel");
 var email = localStorage.getItem("email");
 var picture = localStorage.getItem("picture");
-console.log(username,fname)
 if (jwt == null) {
   window.location.href = './login.html'
 }
 
 
-// function updateprofile() {
-//     const tel = document.getElementById("tel").value;
-//     const fname = document.getElementById("fname").value;
-//     const lname = document.getElementById("lname").value;
-//     const email = document.getElementById("email").value;
-//     const picture = document.getElementById("picture").value;
-//     var data = new FormData();
-//     data.append("tel", tel);
-//     data.append("fname", fname);
-//     data.append("lname", lname);
-//     data.append("email", email);
-//     data.append("picture", picture);
-//     data.append("username", username);
+function updateprofile() {
+    const tel = document.getElementById("tel").value;
+    const fname = document.getElementById("firstname").value;
+    const lname = document.getElementById("lastname").value;
+    const email = document.getElementById("email").value;
+    const picture = document.getElementById("picture").value;
+    var data = new FormData();
+    data.append("tel", tel);
+    data.append("fname", fname);
+    data.append("lname", lname);
+    data.append("email", email);
+    data.append("picture", picture);
+    data.append("username", username);
 
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.withCredentials = true;
+    var xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
 
-//     xhttp.addEventListener("readystatechange", function() {
-//       if(this.readyState === 4) {
-//         console.log(this.responseText);
-//       }
-//     });
+    xhttp.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
 
-//     xhttp.open("POST", "http://localhost:8081/token/v1/profile/update");
-//     xhttp.setRequestHeader("token", jwt);
+    xhttp.open("POST", "http://localhost:8081/token/v1/profile/update");
+    xhttp.setRequestHeader("token", jwt);
 
-//     xhttp.send(data);
-// }
+    xhttp.send(data);
+}
 
 
 function UserProfile() {
